fc63c6d6e5d9ae3bf1bb5464cba15deab47b454a
Valery Kharitonov <val@kharvd.com>
2024-07-31 23:51:55 -0400

    add markdown support

--- app/components/ui/message.tsx
+++ app/components/ui/message.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
 import { ChatCompletionMessage } from "~/lib/schema";
+import Markdown from "react-markdown";
 
 export function Message({ message }: { message: ChatCompletionMessage }) {
-  const formattedText = message.content.split("\n").map((line, index) => (
-    <React.Fragment key={index}>
-      {line}
-      <br />
-    </React.Fragment>
-  ));
-
   return (
     <Alert className="mt-4">
       <AlertTitle>{message.role}</AlertTitle>
-      <AlertDescription>{formattedText}</AlertDescription>
+      <AlertDescription>
+        <Markdown className="prose prose-sm lg:prose-base">
+          {message.content}
+        </Markdown>
+      </AlertDescription>
     </Alert>
   );
 }
