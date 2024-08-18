6ed8180a27105ddfdc781e577911f970764895cc
Valery Kharitonov <val@kharvd.com>
2024-08-18 17:46:14 -0400

    update dialog design

--- app/components/ui/message.tsx
+++ app/components/ui/message.tsx
@@ -2,18 +2,40 @@ import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
 import { ChatCompletionMessage } from "~/lib/schema";
 import Markdown from "react-markdown";
 import React from "react";
+import { cva } from "class-variance-authority";
+import { cn } from "~/lib/utils";
 
+const messageVariants = cva("mt-4 border-0", {
+  variants: {
+    variant: {
+      assistant: "",
+      user: "bg-gray-50 border-0 rounded-2xl w-fit max-w-[90%]",
+    },
+  },
+  defaultVariants: {
+    variant: "user",
+  },
+});
 export const Message = React.memo(
   ({ message }: { message: ChatCompletionMessage }) => {
+    const variant = message.role === "user" ? "user" : "assistant";
     return (
-      <Alert className="mt-4">
-        <AlertTitle>{message.role}</AlertTitle>
-        <AlertDescription>
-          <Markdown className="prose prose-sm max-w-none lg:prose-base">
-            {message.content}
-          </Markdown>
-        </AlertDescription>
-      </Alert>
+      <div className="flex flex-col items-end">
+        <Alert
+          className={cn(
+            messageVariants({
+              variant: message.role === "assistant" ? "assistant" : "user",
+            })
+          )}
+        >
+          {/* <AlertTitle>{message.role}</AlertTitle> */}
+          <AlertDescription>
+            <Markdown className="prose prose-sm max-w-none lg:prose-base">
+              {message.content}
+            </Markdown>
+          </AlertDescription>
+        </Alert>
+      </div>
     );
   }
 );
